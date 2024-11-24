c0bf6669911efb4062350ac6de00b79ed266788d
Wilson Ferreira Junior <wilson.f.jr@gmail.com>
2024-11-24 19:37:55 -0300

    Reescruturação do projeto

--- app/api/auth/routes.py
+++
@@ -1,6 +0,0 @@
-from . import auth
-
-@auth.route('/login')
-def login():
-    return 'Página de login'
-    
--- app/users/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from . import users
-
-@users.route('/register')
-def register():
-    return render_template('users/register.html')
